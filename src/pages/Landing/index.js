8f21c90b6886a4dabe771c45ad72f0686b274ea4
pankaj <pankaj.khanduja2001@gmail.com>
2022-09-06 14:20:22 +0530

    fixed
    fixed

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -41,7 +41,6 @@ import AlertIcon from '../../assets/images/danger.png';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-import axios from "axios";
 const style = {
     position: 'absolute',
     top: '50%',
