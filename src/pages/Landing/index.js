4e4c29ccd237a037bab7480d4c6546191c31ee0f
pankaj <pankaj.khanduja2001@gmail.com>
2022-09-19 23:38:32 +0530

    Updated apk
    Updated apk

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -167,7 +167,7 @@ const Landing = () => {
                     </div>
                     <div className="d-flex">
                         <div className="app-img">
-                        <a href="https://firebasestorage.googleapis.com/v0/b/money-mela.appspot.com/o/moneymelav21.apk?alt=media&token=4b1ef823-9792-4a93-a42c-a028d943ae63">
+                        <a href="https://firebasestorage.googleapis.com/v0/b/money-mela.appspot.com/o/moneymelaV22.apk?alt=media&token=bcdbca49-4e63-4b5d-9aca-51d169fbc6eb">
                                 {/* https://drive.google.com/file/d/1bZbduYX2CfMSIl-tp1GPVi3uXRK0Zbrd/view?usp=sharing */}
                                 <img onClick={handleOpen} src={AndroidImg} />
                             </a>
